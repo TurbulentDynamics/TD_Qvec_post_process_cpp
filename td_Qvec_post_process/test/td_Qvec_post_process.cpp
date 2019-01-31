6eb19665c44e02872617f4b41a81febf03685a0f
Lukas Durfina <lukas.durfina@kistler.com>
2019-01-31 06:33:33 +0100

    Added local download of googletest

---
+++ td_Qvec_post_process/test/td_Qvec_post_process.cpp
@@ -0,0 +1,19 @@
+//
+//  td_Qvec_post_process.m
+//  td_Qvec_post_process
+//
+//  Created by Nile Ó Broin on 24/01/2019.
+//  Copyright © 2019 Nile Ó Broin. All rights reserved.
+//
+
+#include "gmock/gmock.h"
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleMock(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
+
+
